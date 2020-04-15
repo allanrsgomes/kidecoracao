347315849495afa586af70d4b7a3737dc200e923
Allan Gomes <allanhouse2011@hotmail.com>
2020-04-14 23:00:35 -0300

    new footer

--- js/app.js
+++ js/app.js
@@ -39,3 +39,12 @@ debounce = function(func, wait, immediate) {
         animeScroll();
     }, 200));
 }());
+
+
+$(document).ready(function() {
+    $("ul.nav li a[href^='#']").click(function() {
+        $("html, body").stop().animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, 400);
+    });
+});
